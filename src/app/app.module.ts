fbcfcb3ef9b9a28ab4243249b840b33a00d46ce7
normetpiola <normetpiola@gmail.com>
2022-01-05 14:41:58 -0200

    intercepting with in-memry-web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
-import { InMemoryDatabase } from "./in-memory-database"
+//import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+//import { InMemoryDatabase } from "./in-memory-database"
 
 @NgModule({
   declarations: [
@@ -14,7 +14,7 @@ import { InMemoryDatabase } from "./in-memory-database"
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
+    //HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
   providers: [],
   bootstrap: [AppComponent]

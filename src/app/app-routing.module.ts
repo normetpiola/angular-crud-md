68c920efbd1292a6b16074aeb34e78f565bbc34d
normetpiola <normetpiola@gmail.com>
2022-01-19 08:59:45 -0200

    addind default blank route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'reports', loadChildren: './pages/reports/reports.module#ReportsModule' },
   { path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule' },
-  { path: 'entries', loadChildren: './pages/entries/entries.module#EntriesModule' }
+  { path: 'entries', loadChildren: './pages/entries/entries.module#EntriesModule' },
+
+  { path: '', redirectTo: '/reports', pathMatch: 'full'}
 ];
 
 @NgModule({
